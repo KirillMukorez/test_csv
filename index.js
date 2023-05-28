362dff5285f9f13bbd70a0e1fa51c2860d9de077
Kirill <9255119632k@gmail.com>
2023-05-29 00:02:31 +0300

    .

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ import _ from 'lodash';
 
 export default function solution(content){
   // BEGIN
-  console.log(content);
+  const rows = content.trim().split('\r\n').slice(1)
+  const data = rows.map((row) => row.split(','))
+  console.log(`Count ${data.length}`)
+
+  const averageAge = data.reduce((acc, passenger) => acc + Number(passenger[6]), 0) / data.length
+
+  const ports = data.reduce((acc, passenger) => {
+    const count = _.get(acc, passenger[12], 0) + 1
+    return { ...acc, [passenger[12]] : count}
+  }, {})
+  console.log(`Ports: ${JSON.stringify(ports)}`)
   // END
 }
